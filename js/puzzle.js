3b571ddeff65fc9c75776e4d88b8811d9ca4a500
yixiao <853117762@qq.com>
2019-11-24 23:34:29 +0800

    拼图算法 4*4以上有待优化

--- js/puzzle.js
+++ js/puzzle.js
@@ -6,12 +6,14 @@ class Puzzle {
      * 
      * @param {contain} 拼图渲染位置
      * @param config
-     * @param {size}  ·N*N的拼图 
+     * @param {size}  ·N*N的拼图 最大为8 
      * @param {image} 拼图所用图片
      * @param {animate} 动画速度
      * 
      */
     constructor(contain, config) {
+        //固定的位置表示符号 按ascii码大小排列
+        this.puzzleConst = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz{}';
         if(!contain || (contain && !contain.offsetWidth)) {
             console.error('请传入正确的dom');
             return;
@@ -25,9 +27,8 @@ class Puzzle {
         this.yStep = this.contain.offsetHeight / this.size;  //每个格子分配高度
         this.position = [];
         this.orders = [];
+        this.blank = this.puzzleConst[this.size * this.size - 1];
 
-        //位置和数值的映射关系
-        this.originPositon = {};
         //记录当前顺序
         this.currentOrders = [];
 
@@ -38,25 +39,23 @@ class Puzzle {
     }
 
     /**
-     * 根据size 初始化坐标位置和拼图顺序
+     * 根据size 初始化坐标位置和拼图顺序 eg: size === 3
      * 向右为x轴正方向
      * 向下为y轴正方向[x, y]
      * eg: [                                    [                  
-     *          [0,0],[1,0],[2,0],                  0,1,2,
-     *          [0,1],[1,1],[2,1],    ====>         3,4,5,
-     *          [0,2],[1,2],[2,2],                  6,7,8
+     *          [0,0],[1,0],[2,0],                  '0','1','2',
+     *          [0,1],[1,1],[2,1],    ====>         '3','4','5',
+     *          [0,2],[1,2],[2,2],                  '6','7','8'
      *     ]                                    ]
      * */
     positionInit() {
         for(let i = 0; i < this.size; i++) { //y
             for(let j = 0; j < this.size; j++) { //x
-                this.position.push([j, i]);  
-                this.orders.push(i * this.size + j);
-                //图片顺序 对应到的位置
-                this.originPositon = Object.assign(this.originPositon, {
-                    [`${ i * this.size + j }`]: [j, i]
-                })
-                this.currentOrders.push(i * this.size + j);
+                let key = this.puzzleConst[i * this.size + j], 
+                position = [j, i];
+                this.position.push(position); 
+                this.orders.push(key);
+                this.currentOrders.push(key);
             }
         }
     }
@@ -64,34 +63,115 @@ class Puzzle {
     //初始化渲染拼图
     initRender() {
         this.contain.innerHTML = '';
-        this.orders.forEach((item)=>{
-            let _position = this.position[item];
+        this.orders.forEach((item, index)=>{
+            //item拼图块的标识，index拼图块的位置
+            let _position = this.position[index];
             let dom = document.createElement('div');
-            dom.innerText = item;
+            dom.innerText = index;
             dom.className = `puzzle-game position-${ item }`;
             dom.style.left = `${ _position[0] * this.xStep }px`;
             dom.style.top = `${ _position[1] * this.yStep }px`;
+            dom.style.width = `${ this.xStep }px`;
+            dom.style.height = `${ this.yStep }px`;
+            dom.style.lineHeight = `${ this.yStep }px`;
             dom.style.transition = `left ${this.animate}s, top ${this.animate}s`;
             dom.style.backgroundPosition = `-${ _position[0] * this.xStep }px -${ _position[1] * this.yStep }px`;
-            if(_position[0] == 2 && _position[1] == 2) {
+            if(item === this.blank) {
                 dom.style.backgroundImage = 'none';
+                dom.style.borderColor = 'transparent';
+                dom.innerText = '';
             }
             this.contain.appendChild(dom);
         })
+        
     }
 
     //改变拼图位置
     render() {
         //当前排序和坐标系重新建立映射关系
+        // console.log(this.currentOrders, this.orders);
         this.currentOrders.forEach((item, index) => {
             let _position = this.position[index];
             let dom = document.querySelector('.position-' + item);
             dom.style.left = `${ _position[0] * this.xStep }px`;
             dom.style.top = `${ _position[1] * this.yStep }px`;
-            this.originPositon = Object.assign(this.originPositon, {
-                [`${ item }`]: _position
-            })
         })
+    }
+
+    //交换两张拼图的位置
+    transposer(order, key1, key2) {
+        let orders = this.deepCopy(order);
+        let placeholder = orders[key1];
+        orders[key1] = orders[key2];
+        orders[key2] = placeholder;
+        return orders;
+    }
+
+    /**
+     * 拼图的上下左右移动
+     * @param right 向右
+     *        left 
+     *        up
+     *        down
+     * */
+    actionPuzzle(directive) {
+        if(!directive) {
+            return
+        }
+
+        //查询当前空白位置和周围拼图位置，并执行移动
+        //空白方块值
+        let blankV = this.blank;
+        let blankIndex = this.currentOrders.indexOf(blankV);
+        let blankPoint = this.getPosition(blankIndex);
+        //空白方块周围拼图位置
+        let right;
+        let left;
+        let up;
+        let down;
+        let length = this.currentOrders.length;
+        for(let i = 0; i < length; i++) {
+            let _itemPoint = this.getPosition(i);
+            if(_itemPoint.x === blankPoint.x) {
+                if(_itemPoint.y - 1 === blankPoint.y) {
+                    down = i;
+                }
+                if(_itemPoint.y + 1 === blankPoint.y) {
+                    up = i;
+                }
+            } else if(_itemPoint.y === blankPoint.y) {
+                if(_itemPoint.x - 1 === blankPoint.x) {
+                    right = i;
+                }
+                if(_itemPoint.x + 1 === blankPoint.x) {
+                    left = i;
+                }
+            }
+        }
+
+        switch(directive) {
+            case 'right':
+                if(left >= 0) {
+                    this.currentOrders = this.transposer(this.currentOrders, left, blankIndex)
+                }
+                break;
+            case 'left':
+                if(right >= 0) {
+                    this.currentOrders = this.transposer(this.currentOrders, right, blankIndex)
+                }
+                break;
+            case 'up':
+                if(down >= 0) {
+                    this.currentOrders = this.transposer(this.currentOrders, down, blankIndex)
+                }
+                break;
+            case 'down':
+                if(up >= 0) {
+                    this.currentOrders = this.transposer(this.currentOrders, up, blankIndex)
+                }
+                break;
+        }
+        this.render();
     }
 
     //游戏初始化
@@ -100,7 +180,7 @@ class Puzzle {
         this.initRender();
 
         //添加滑动事件  //操作拼图滑动
-
+        this.attachEvent(this.contain);
     }
     
     //生成可还原拼图排列
@@ -110,7 +190,7 @@ class Puzzle {
         while(!this.checkPuzzle(newPuzzleArr)) {
             newPuzzleArr = this.randomPuzzle();
         }
-        let orders = [...newPuzzleArr, this.size * this.size - 1];
+        let orders = [...newPuzzleArr, this.blank];
 
         this.currentOrders = orders;
 
@@ -120,12 +200,9 @@ class Puzzle {
 
     //随机生成拼图排列 最后一个位置固定，不参与随机
     randomPuzzle() {
-        let randomArr = [],
-            origin = [], 
-            length = this.size * this.size - 1;
-        for(let i = 0; i < length; i++) {
-            origin.push(i);
-        }
+        let randomArr = [], 
+            length = this.orders.length - 1,
+            origin = this.deepCopy(this.orders).slice(0, length);
         while (randomArr.length < length) {
             let index = Math.floor(Math.random() * origin.length);
             randomArr.push(origin[index]);
@@ -182,5 +259,252 @@ class Puzzle {
     }
 
     //事件绑定器
+    attachEvent(target) {
+        let startX = 0;
+        let startY = 0;
+        target.addEventListener('touchstart', (event)=> {
+            startX = event.touches[0].pageX;
+            startY = event.touches[0].pageY;
+        });
+        target.addEventListener('touchend', (event)=> {
+            var spanX = event.changedTouches[0].pageX - startX;
+            var spanY = event.changedTouches[0].pageY - startY;
+
+            if(Math.abs(spanX) > Math.abs(spanY)){      //认定为水平方向滑动
+                if(spanX > 30){         //向右
+                    this.actionPuzzle('right')
+                } else if(spanX < -30){ //向左
+                    this.actionPuzzle('left')
+                }
+            } else {                                    //认定为垂直方向滑动
+                if(spanY > 30){         //向下
+                    this.actionPuzzle('down')
+                } else if (spanY < -30) {//向上
+                    this.actionPuzzle('up')
+                }
+            }
+        });
+    }
+
+    /**
+     *  拼图还原步骤算法
+     */
+    autoCalcPuzzle(order) {
+        let orders = order || this.currentOrders;
+        let opens = [];
+        let closeMap = new Map();
+
+        //估值函数
+        let curr = orders.join('');
+        let nodeValue = this.getDistanceTotal(curr);
+        opens.push({
+            v: nodeValue,
+            step: 0,
+            order: curr
+        })
+
+        //a*搜索
+        while (opens.length !== 0) {
+            //获取opens中估值最小的节点
+            let currentNode = opens.splice(this.minOfArray(opens), 1)[0];
+            // console.log(currentNode, opens);
+            closeMap.set(currentNode.order, currentNode);
+
+            //判断是否到达最终状态
+            if(currentNode.order == this.orders.join('')) {
+                console.log('寻路成功！！！！', currentNode);
+                let paths = this.getPath(currentNode, closeMap);
+                let pathsNames = [];
+                for(let i = paths.length - 1; i > 0; i--) {
+                    pathsNames.push(this.isChange(paths[i].order, paths[i-1].order));
+                }
+                console.log(pathsNames);
+                break;
+            }
+
+            //未到达最终状态，则继续扩展子节点
+            let childNodes = this.getNextOrders(currentNode.order);
+            // console.log(childNodes);
+            childNodes.forEach(item => {
+                //子节点筛选 路径如果扩展过则去掉
+                let openIndex = this.indexOfArray(opens, item);
+                // console.log(openIndex);
+
+                if(!closeMap.has(item)) {
+                    let node = {
+                        v: this.getDistanceTotal(item) + currentNode.step + 1,
+                        step: currentNode.step + 1,
+                        order: item,
+                        parent: currentNode
+                    }
+
+                    //将新的节点按估值大小插入opens列表里面
+                    //如果opens中存在，则对比两个的步数和估值，
+                    //如果步数不一样，取步数小的，如果步数一样，取估值小地
+                    if(openIndex == -1) {
+                        opens.push(node)
+                    } else {
+                        let opnesValue = opens[openIndex];
+                        if(opnesValue.v > node.v) {
+                            opens.splice(openIndex, 1, node);
+                        }
+                    }
+                }
+            })
+
+            if(opens.length == 0) {
+                console.log('寻路失败')
+            }
+        }
+    }
+
+    //状态距离还原状态位置 //空白方块不计算位置距离
+    getDistanceTotal(order) {
+        let orders = order.split('');
+        let total = 0;
+        orders.forEach((item, index)=> {
+            let itemIndex = this.orders.indexOf(item);
+            if(item !== this.blank) {
+                total += this.getDistance(itemIndex, index);
+            }
+        })
+        return total
+    }
+
+    //坐标位置拼图距离应还原位置的步数
+    getDistance(origin, point){
+        let op = this.getPosition(origin), //该拼图还原位置
+           cp = this.getPosition(point);  //该拼图当前位置
+        return Math.abs(op.x - cp.x) + Math.abs(op.y - cp.y)
+    }
     
+    //查询数组中最小值
+    minOfArray(arr) {
+        let index = 0;
+        let length = arr.length;
+        let min = arr[0];
+        for(let i = 0; i < length; i++) {
+            if(arr[i].v < min.v) {
+                index = i;
+                min = arr[i];
+            }
+        }
+        return index
+    }
+
+    //查询数组中是否存在
+    indexOfArray(arr, one) {
+        let index = -1, length = arr.length;
+        for(let i = 0; i < length; i++) {
+        // console.log(arr[i].order, one)
+            if(arr[i].order === one) {
+                index = i;
+            }
+        }
+        return index
+    }
+ 
+
+    //扩展目标函数的子节点
+    getNextOrders(order) {
+        let curr = order.split('');
+        let blankValue = this.blank;
+        let length = curr.length;
+        let blank;
+        let blankOrder; //输入节点空白位置
+        for(let i = 0; i < length; i++) {
+           if(curr[i] === blankValue) {
+              blank = this.getPosition(i);
+              blankOrder = i;
+              break;
+           }
+        }
+        // console.log(blank, blankOrder);
+        //查询与当前空白节点只有1步的节点
+        let childNodes = [];
+        curr.forEach((item, index) => {
+            let aroundNode = this.getPosition(index);
+            if(Math.abs(aroundNode.x - blank.x) + Math.abs(aroundNode.y - blank.y) == 1) {
+                childNodes.push(this.transposer(curr, blankOrder, index).join(''));
+            }
+        })
+        // console.log(JSON.stringify(curr), JSON.stringify(childNodes));
+        return childNodes
+    }
+
+    //路径拼接
+    getPath(node) {
+        let paths = [];
+        paths.push(node);
+        if(node.parent) {
+            return paths.concat(this.getPath(node.parent));
+        } else {
+            return paths
+        }
+    }
+
+    /**
+     * 输入两个数组
+     * 如果可以通过移动一次则返回 上下左右 ==》 u，d，l，r
+     * 不能一步变换得到则返回 null
+     */
+    isChange(current, next) {
+        let currArr = current.split('');
+        let nextArr = next.split('');
+        let differents = [];
+        let length = currArr.length;
+        for(let i = 0; i < length; i++) {
+            if(currArr[i] !== nextArr[i]) {
+                let obj = {
+                    index: i,
+                    point: null
+                }
+                if(currArr[i] == this.blank) {
+                    obj.point = 'start';
+                }
+                if(nextArr[i] == this.blank) {
+                    obj.point = 'end';
+                }
+                differents.push(obj);
+            }
+        }
+        if(differents.length !== 2) {
+            console.log('不能靠一步移动得到');
+            return null
+        }
+        let s, e;
+        differents.forEach(item => {
+            if(item.point == 'start') {
+                s = this.getPosition(item.index);  
+            }
+            if(item.point == 'end') {
+                e = this.getPosition(item.index);  
+            }
+        })
+        let direction = {
+            x: e.x - s.x,
+            y: e.y - s.y
+        }
+        if(direction.x == 0) {
+            if(direction.y == -1) {
+                console.log('向下')
+                return 'd'
+            }
+            if(direction.y == 1) {
+                console.log('向上')
+                return 'u'
+            }
+        }
+        if(direction.y == 0) {
+            if(direction.x == -1) {
+                console.log('向右')
+                return 'r'
+            }
+            if(direction.x == 1) {
+                console.log('向左')
+                return 'l'
+            }
+        }
+
+    }
 }
